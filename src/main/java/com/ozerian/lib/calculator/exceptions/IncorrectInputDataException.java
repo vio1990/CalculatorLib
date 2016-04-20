bcf46306f27a3da6af10f10eb80e0086c24e09e7
Ivan Ozerian <vio1990@ukr.net>
2016-04-20 18:26:25 +0300

    JavaDoc addition to Exceptions.

--- src/main/java/com/ozerian/lib/calculator/exceptions/IncorrectInputDataException.java
+++ src/main/java/com/ozerian/lib/calculator/exceptions/IncorrectInputDataException.java
@@ -1,5 +1,8 @@
 package com.ozerian.lib.calculator.exceptions;
 
+/**
+ * Exception throws when there are problems with input data after some methods and operations.
+ */
 public class IncorrectInputDataException extends Exception {
 
     public IncorrectInputDataException(String message) {
--- src/main/java/com/ozerian/lib/calculator/exceptions/WrongInputDataException.java
+++ src/main/java/com/ozerian/lib/calculator/exceptions/WrongInputDataException.java
@@ -1,5 +1,8 @@
 package com.ozerian.lib.calculator.exceptions;
 
+/**
+ * Exception throws when there are problems with input data initially.
+ */
 public class WrongInputDataException extends Exception {
 
     public WrongInputDataException(String message) {
